787a43affcafe524e4137ce79eae2f490ea8c519
Craig Pastro <pastro.craig@gmail.com>
2022-12-05 19:45:55 -0800

    Update shasum (#2)

--- Formula/scryer-prolog.rb
+++ Formula/scryer-prolog.rb
@@ -2,7 +2,7 @@ class ScryerProlog < Formula
   desc "A modern Prolog implementation written mostly in Rust."
   homepage "https://github.com/mthom/scryer-prolog"
   url "https://github.com/craigpastro/scryer-prolog/releases/download/v0.9.2/scryer-prolog-0.9.2-x86_64-apple-darwin.tar.gz"
-  sha256 "ea6a14fc02bec565bb490eb1fe8702484c1a02ed"
+  sha256 "60381b068d03f48746fec1fc5539d23a7a7c07510b2f165ffcd7e381d036aae7"
   version "0.9.2"
 
   def install
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Homebrew Tap
+
+Print the SHA checksum of a binary with:
+```console
+shasum -a 256 <binary>
+```
